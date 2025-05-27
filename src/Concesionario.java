9e92989e5d558dbff799c4fb5e5a844e51e66bd2
svalenciahiud <santiago.valenciah@est.iudigital.edu.co>
2025-05-26 22:36:56 -0500

    Se completo la condicion del if para encontrar el vehiculo que se desea vender

--- src/Concesionario.java
+++ src/Concesionario.java
@@ -15,7 +15,7 @@ public class Concesionario {
         Vehiculo vehiculoVendido = null;
         // Buscar el vehículo en el inventario
         for (Vehiculo v : inventario) {
-            if (v.getCodigo().equals(codigo*******)) {
+            if (v.getCodigo().equals(codigoVehiculo)) {
                 vehiculoVendido = v;
                 break;
             }
